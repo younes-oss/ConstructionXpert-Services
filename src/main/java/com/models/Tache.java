7f2707b7298167a84bf5658d93d40293bbea67fe
younes-oss <aitchattouyounes@gmail.com>
2025-03-22 14:46:43 +0100

    la creation du classe modele tache

---
+++ src/main/java/com/models/Tache.java
@@ -0,0 +1,75 @@
+package com.models;
+
+import java.util.Date;
+
+public class Tache {
+    private int id;
+    private String nom;
+    private String description;
+    private Date dateDebut;
+    private Date dateFin;
+    private int projetId; // Lien avec le projet
+
+    // Constructeur vide
+    public Tache() {}
+
+    // Constructeur avec paramètres
+    public Tache(int id, String nom, String description, Date dateDebut, Date dateFin, int projetId) {
+        this.id = id;
+        this.nom = nom;
+        this.description = description;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.projetId = projetId;
+    }
+
+    // Getters et Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public Date getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(Date dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public Date getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(Date dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public int getProjetId() {
+        return projetId;
+    }
+
+    public void setProjetId(int projetId) {
+        this.projetId = projetId;
+    }
+}
